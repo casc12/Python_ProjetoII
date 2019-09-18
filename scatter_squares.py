1bc94d9086870e70710c9add87d89f109e0b24f0
Carlos Augusto de Sousa <carlos_sousamn@yahoo.com.br>
2019-09-17 22:46:34 -0300

    Salvando seus gráficos automaticamente

--- scatter_squares.py
+++ scatter_squares.py
@@ -17,4 +17,4 @@ plt.tick_params(axis='both',which='major', labelsize=14)
 # Define o título do gráfico e nomeia os eixos
 plt.axis([0, 1100, 0, 1100000])
 
-plt.show()
+plt.savefig('squares_plot.png', bbox_inches='tight')
