54cd726b2250e02526295df2d155552efdc37ba1
Carlos Augusto de Sousa <carlos_sousamn@yahoo.com.br>
2019-09-18 13:19:31 -0300

    Lançando dois Dados

--- dice_visual.py
+++ dice_visual.py
@@ -26,7 +26,7 @@ for value in range(1, max_result + 1):
 # Visualiza  os resultados
 hist = pygal.Bar()
 
-hist.title = "Results of rolling one D6 1000 times."
+hist.title = "Results of rolling two D6 1000 times."
 hist.x_labels = ['2', '3' , '4' , '5', '6', '7', '8', '9', '10', '11', '12']
 hist.x_title = "Reseult"
 hist.y_title = "Frequency of Result"
