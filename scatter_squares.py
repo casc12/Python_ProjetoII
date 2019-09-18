19ffdff6370b0137139cb5a1aaf36d86f2d582f3
Carlos Augusto de Sousa <carlos_sousamn@yahoo.com.br>
2019-09-17 21:55:33 -0300

    Plotando uma série de pontos com scatter().

--- scatter_squares.py
+++ scatter_squares.py
@@ -1,7 +1,10 @@
 import matplotlib.pyplot as plt
 
 
-plt.scatter(2, 4, s =200)
+x_values = [1, 2, 3, 4, 5]
+y_values = [1, 4, 9, 16, 25]
+
+plt.scatter(x_values, y_values, s =100)
 
 # Define o título do gráfico e nomeia os eixos
 plt.title("Square Numbers", fontsize=14)
