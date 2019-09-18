3720f73fc0de99ca9a8c9542c1d8af70bf68b323
Carlos Augusto de Sousa <carlos_sousamn@yahoo.com.br>
2019-09-18 07:56:09 -0300

    Gerando vários passeios aleatórios

--- rw_visual.py
+++ rw_visual.py
@@ -3,8 +3,13 @@ import matplotlib.pyplot as plt
 from random_walk import RandomWalk
 
 
-# Cria um passeio aleatório
-rw = RandomWalk()
-rw.fill_walk()
-plt.scatter(rw.x_values, rw.y_values, s=15)
-plt.show() 
+# continua Criando novos passeios aleatórios 
+while True:
+	# Cria um passeio aleatório
+	rw = RandomWalk()
+	rw.fill_walk()
+	plt.scatter(rw.x_values, rw.y_values, s=15)
+	plt.show()
+	keep_running = input("Make another walk? (y/n): ")
+	if keep_running == 'n':
+		break 
