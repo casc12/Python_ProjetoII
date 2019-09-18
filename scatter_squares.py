bb83993b864b930a01d19511d94484b61dc4f95f
Carlos Augusto de Sousa <carlos_sousamn@yahoo.com.br>
2019-09-17 21:44:57 -0300

    scatter colocando rotulos nos eixos!

--- scatter_squares.py
+++ scatter_squares.py
@@ -1,5 +1,13 @@
 import matplotlib.pyplot as plt
 
 
-plt.scatter(2, 4)
+plt.scatter(2, 4, s =200)
+
+# Define o título do gráfico e nomeia os eixos
+plt.title("Square Numbers", fontsize=14)
+plt.xlabel("Value", fontsize=14)
+plt.ylabel("Square of Value", fontsize=14)
+
+#Define o tamanho dos rótulos das marcações
+plt.tick_params(axis='both',which='major', labelsize=14)
 plt.show()
