22c9334f3dd028fe530a76b60030a0c2907707c2
Carlos Augusto de Sousa <carlos_sousamn@yahoo.com.br>
2019-09-21 20:47:16 -0300

    Extraindo Dados.

--- highs_lows.py
+++ highs_lows.py
@@ -5,7 +5,12 @@ filename = 'sitka_weather_07-2014.csv'
 with open(filename) as f:
 	reader = csv.reader(f)
 	header_row = next(reader)
-	print(header_row)
-	for index, column_header in enumerate(header_row):
-		print(index, column_header)
+	#print(header_row)
+	highs = []
+	for row in reader:
+		highs.append(row[1])
+		
+		
+		
+	print(highs)
 	
