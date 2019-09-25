0cba06c701c68ebd1ebe7d0f59c7b714289a0de2
Carlos Augusto de Sousa <carlos_sousamn@yahoo.com.br>
2019-09-24 21:24:57 -0300

    Plotando uma segunda série de Dados

--- highs_lows.py
+++ highs_lows.py
@@ -12,7 +12,7 @@ with open(filename) as f:
 	reader = csv.reader(f)
 	header_row = next(reader)
 	#print(header_row)
-	dates,highs = [], []
+	dates,highs, lows = [], [], []
 	for row in reader:
 		current_date = datetime.strptime(row[0], "%Y-%m-%d")
 		dates.append(current_date)
@@ -21,6 +21,9 @@ with open(filename) as f:
 		high = int(row[1])
 		highs.append(high)
 		
+		low = int(row[3])
+		lows.append(low)
+		
 		
 		
 	#print(highs)
@@ -28,9 +31,10 @@ with open(filename) as f:
 # Faz a plotagem dos dados
 fig = plt.figure(dpi =128, figsize=(10, 6))
 plt.plot(dates, highs, c='red')
+plt.plot(dates, lows, c='blue')
 
 # Formata o gráfico 
-plt.title("Daily high temperatures - 2014", fontsize=24)
+plt.title("Daily high and low temperatures - 2014", fontsize=24)
 plt.xlabel('', fontsize=16)
 fig.autofmt_xdate()
 plt.ylabel("Temperature (F)", fontsize=16)
