31a002e899763931550a4fcb81d18c8f12a8ffd5
Carlos Augusto de Sousa <carlos_sousamn@yahoo.com.br>
2019-09-25 18:37:04 -0300

    Agrupando os países de acordo com a sua população

--- world_population.py
+++ world_population.py
@@ -24,12 +24,29 @@ for pop_dict in pop_data:
 		if code:
 			#print(code+": "+str(population))
 			cc_poplations[code] = population
-		"""else:
-			print('ERROR - '+ country_name)"""
+		else:
+			print('ERROR - '+ country_name)
+
+#Agrupa países em três níveis populacionais
+cc_pops_1, cc_pops_2, cc_pops_3 = {}, {}, {}
+for cc, pop in cc_poplations.items():
+	if pop < 10000000:
+		cc_pops_1[cc] = pop
+		
+	elif pop < 1000000000:
+		cc_pops_2[cc]=pop
+		
+	else:
+		cc_pops_3[cc]= pop
+# Vê quantos países estão em cada nível 
+print(len(cc_pops_1), len(cc_pops_1), len(cc_pops_3))
 
 wm = World()
 wm.title='World Population in 2010, by Country'
-wm.add('2010', cc_poplations)
+#wm.add('2010', cc_poplations)
+wm.add('0-10m', cc_pops_1)
+wm.add('10m-1bn', cc_pops_2)
+wm.add('>1bn', cc_pops_3)
 
 
 wm.render_to_file('world_population.svg')
