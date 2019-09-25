143faba58e3646e314b89e5ad5befa59fc765b2b
Carlos Augusto de Sousa <carlos_sousamn@yahoo.com.br>
2019-09-25 14:38:59 -0300

    Verificação de erros

--- highs_lows.py
+++ highs_lows.py
@@ -6,7 +6,7 @@ from matplotlib import pyplot as plt
 
 
 # Obtém as temperaturas Máximas
-filename = 'sitka_weather_2014.csv'
+filename = 'death_valley_2014.csv'
 
 with open(filename) as f:
 	reader = csv.reader(f)
@@ -14,15 +14,25 @@ with open(filename) as f:
 	#print(header_row)
 	dates,highs, lows = [], [], []
 	for row in reader:
-		current_date = datetime.strptime(row[0], "%Y-%m-%d")
-		dates.append(current_date)
-		
-		
-		high = int(row[1])
-		highs.append(high)
-		
-		low = int(row[3])
-		lows.append(low)
+		try:
+			current_date = datetime.strptime(row[0], "%Y-%m-%d")
+			
+			
+			
+			high = int(row[1])
+			
+			
+			
+			low = int(row[3])
+			
+		except ValueError:
+			print(current_date, 'missing data')
+			
+		else:
+			dates.append(current_date)
+			highs.append(high)
+			lows.append(low)
+			
 		
 		
 		
@@ -35,7 +45,8 @@ plt.plot(dates, lows, c='blue', alpha=0.5)
 plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
 
 # Formata o gráfico 
-plt.title("Daily high and low temperatures - 2014", fontsize=24)
+title = "Daily high and low temperatures - 2014\nDeath Valley, CA"
+plt.title(title, fontsize=24)
 plt.xlabel('', fontsize=16)
 fig.autofmt_xdate()
 plt.ylabel("Temperature (F)", fontsize=16)
