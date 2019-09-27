1d571e5dd996d87a79f91e16ae0c2803a13503b3
Carlos Augusto de Sousa <carlos_sousamn@yahoo.com.br>
2019-09-27 17:51:00 -0300

    Plotando os Dados

--- python_repos.py
+++ python_repos.py
@@ -35,10 +35,20 @@ print("\nKeys:", len(repo_dict))
 
 for key in sorted(repo_dict.keys()):
 	print(key)"""
-names, stars = [], []
+print("Number of items: ", len(repo_dicts))
+
+names, plot_dicts = [], []
 for repo_dict in repo_dicts:
 	names.append(repo_dict['name'])
-	stars.append(repo_dict['stargazers_count'])
+	#stars.append(repo_dict['stargazers_count'])
+	description = repo_dict['description']
+	if not description:
+		description = "No description provided."
+	plot_dict = {
+	'value': repo_dict['stargazers_count'],
+	'label': description
+	}
+	plot_dicts.append(plot_dict)
 # Cria a Visualização
 my_style = LS('#333366', base_style=LCS)
 
@@ -58,5 +68,6 @@ chart= pygal.Bar(my_config, style=my_style)
 chart.title = 'Most-Starred Python Projects on GitHub'
 chart.x_labels = names
 
-chart.add('', stars)
+chart.add('', plot_dicts)
+print(plot_dict)
 chart.render_to_file('python_repos.svg')
