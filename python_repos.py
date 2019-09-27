c0d3201a20389565b3f308a2c0d80ed708853d08
Carlos Augusto de Sousa <carlos_sousamn@yahoo.com.br>
2019-09-26 23:25:43 -0300

    Visualizando os repositórios usando Pygal

--- python_repos.py
+++ python_repos.py
@@ -1,5 +1,6 @@
 import requests
-
+import pygal
+from pygal.style import LightColorizedStyle as LCS, LightenStyle as LS
 
 # Faz a chamda a API e armazena a resposta
 url="https://api.github.com/search/repositories?q=language:python&sort=stars"
@@ -14,7 +15,7 @@ print("Total repositories:", response_dict['total_count'])
 
 #Explora Informações sobre os repositórios
 repo_dicts = response_dict['items']
-print("Repositories returned:", len(repo_dicts))
+"""print("Repositories returned:", len(repo_dicts))
 
 
 # Analisa o Primeiro repositório
@@ -30,7 +31,19 @@ for repo_dict in repo_dicts:
 	print('Created: ', repo_dict['created_at'])
 	print('Update: ',repo_dict['updated_at'])
 	print('Discription: ', repo_dict['description'])
-"""print("\nKeys:", len(repo_dict))
+print("\nKeys:", len(repo_dict))
 
 for key in sorted(repo_dict.keys()):
 	print(key)"""
+names, stars = [], []
+for repo_dict in repo_dicts:
+	names.append(repo_dict['name'])
+	stars.append(repo_dict['stargazers_count'])
+# Cria a Visualização
+my_style = LS('#333366', base_style=LCS)
+chart= pygal.Bar(style=my_style, x_label_rotation=45, show_legend=False)
+chart.title = 'Most-Starred Python Projects on GitHub'
+chart.x_labels = names
+
+chart.add('', stars)
+chart.render_to_file('python_repos.svg')
